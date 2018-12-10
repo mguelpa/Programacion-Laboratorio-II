dc92040eb38485ea21f029a1d1131abc8b2ec5dc
Maximiliano <mguelpa@gmail.com>
2018-12-10 11:19:56 -0300

    -

---
+++ E01/E01/ArrayNumeros.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01
+{
+    public static class ArrayNumeros
+    {
+        public static string[] SolicitarNumeros(int ingresos, int intentos, string msgError)
+        {
+            int i, j = intentos;
+            string[] aux = new string[ingresos];
+
+            Console.WriteLine("ingrese {0} numero(s)", ingresos);
+            for (i = 0; i < ingresos; i++)
+            {
+                aux[i] = Console.ReadLine();
+                while (!IsNumeric(aux[i]) && intentos > 0)
+                {
+                    Console.WriteLine(msgError + "({0})", intentos);
+                    aux[i] = Console.ReadLine();
+                    intentos--;
+                }
+
+                if (intentos < 0)
+                    return null;
+
+                intentos = j;
+            }
+            return aux;
+        }
+
+
+        public static bool IsNumeric(string number)
+        {
+            bool auxReturn = true;
+            int count = 0;
+            int flag = 0;
+
+            foreach (char c in number)
+            {            
+                if (c == '-' && count == 0)
+                {
+                    flag++;
+                    continue;
+                }
+                if ((c < '0' || c > '9'))
+                    auxReturn = false;
+                count++;
+            }
+            if(flag > 0 && count == 0)
+            {
+                auxReturn = false;
+            }
+            return auxReturn;
+        }
+
+        public static void MostrarNumeros(string[] numeros)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Se ingreso el conjunto: { ");
+            foreach (string s in numeros)
+            {
+                sb.Append(s + ", ");
+            }
+            sb.Remove(sb.Length-2, 2);
+            sb.Append(" }");
+            Console.WriteLine(sb.ToString());
+        }
+
+    }
+}
--- E01/E01/Program.cs
+++ E01/E01/Program.cs
@@ -11,58 +11,17 @@ namespace E01
         static void Main(string[] args)
         {
             Console.Title = "EJERCICIO 01";
+            //////////////////////////////
+
+
 
             string[] numeros = new string[5];
 
-            numeros = solicitarNumeros(5, 3, "reingrese un valor numerico");
+            numeros = ArrayNumeros.SolicitarNumeros(5, 3, "ha ingresado un valor no valido, intente nuevamente: ");
 
             if (numeros != null)
-            {
-                Console.WriteLine("\n\nSe ingreso: ");
-                foreach (string s in numeros)
-                    Console.WriteLine(s);
-            }
+                ArrayNumeros.MostrarNumeros(numeros);
 
-        }
-
-        public static string[] solicitarNumeros(int ingresos, int intentos, string msgError)
-        {
-            int i, j = intentos;
-            string[] aux = new string[ingresos];
-
-            Console.WriteLine("ingrese {0} numero(s)", ingresos);
-            for (i = 0; i < ingresos; i++)
-            {
-                aux[i] = Console.ReadLine();
-                while (!IsNumeric(aux[i]) && intentos > 0)
-                {
-                    Console.WriteLine(msgError + "({0})", intentos);
-                    aux[i] = Console.ReadLine();
-                    intentos--;
-                }
-
-                if (intentos == 0)
-                    return null;
-
-                intentos = j;
-            }
-            return aux;
-        }
-
-        public static bool IsNumeric(string number)
-        {
-            int i = 0;
-
-            foreach (char c in number)
-            {
-                if (c == '-' && i > 0)
-                    return false;
-
-                if ((c < '0' || c > '9') && (c != '-'))
-                    return false;
-                i++;
-            }
-            return true;
         }
     }
 }
